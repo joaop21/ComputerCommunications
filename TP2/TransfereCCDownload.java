e0e044d711a922acdcd335a5dfd9cfd202d3921e
joaop21 <jpsilva9898@gmail.com>
2019-04-16 00:17:18 +0100

    erro no download

--- TP2/TransfereCCDownload.java
+++ TP2/TransfereCCDownload.java
@@ -86,7 +86,7 @@ class TransfereCCDownload extends Thread{
         }
 
         // envia ACK
-        PDU ack = new PDU(2, 1, 1024, new String(), false, false, false, true, new byte[0]);
+        PDU ack = new PDU(2, 1, 1024, new String(), false, false, true, false, new byte[0]);
         agente.sendPDU(syn,addressDest,7777);
     }
 
